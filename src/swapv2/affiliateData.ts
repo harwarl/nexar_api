3aef67ab694a9adf390e2c8ea6cd18800967a7aa
harwarl <harwarl87@gmail.com>
2025-08-12 15:56:26 +0100

    chore: clean up affiliate links

--- src/swapv2/affiliateData.ts
+++ src/swapv2/affiliateData.ts
@@ -7,81 +7,75 @@ export interface AffiliateProviderConfig {
   apiSecret?: string;
   endpoints: {
     tokens: string;
-    getTransactions: string;
-    getTransactionById: string;
-    createExchangeTxn: string;
-    getMiniumAmount?: string;
-    confirmTransaction?: string;
     chains?: string;
+    getTransactions?: string;
+    getTransactionById?: string;
+    createExchangeTxn?: string;
+    getMinimumAmount?: string;
+    confirmTransaction?: string;
   };
 }
 
 export const AFFILIATE_PROVIDERS: AffiliateProviderConfig[] = [
   {
     name: 'exolix',
-    baseUrl: '',
+    baseUrl: '', // e.g., 'https://api.exolix.com/v1/'
     apiKey: process.env.EXOLIX_API_KEY || '',
     endpoints: {
       tokens: 'currencies',
       chains: 'currencies/networks',
       getTransactions: 'transactions',
-      getTransactionById: 'transactions/', // expects an Id
-      createExchangeTxn: 'transactions', // POST
+      getTransactionById: 'transactions/', // expects ID as param
+      createExchangeTxn: 'transactions', // POST with body
     },
   },
   {
     name: 'fixed_float',
-    baseUrl: '',
+    baseUrl: '', // e.g., 'https://api.fixedfloat.com/v1/'
     apiKey: process.env.FIXED_FLOAT_API_KEY || '',
     apiSecret: process.env.FIXED_FLOAT_API_SECRET || '',
     endpoints: {
       tokens: 'ccies',
       chains: 'currencies/networks',
-      getTransactions: '',
-      getTransactionById: '',
-      getMiniumAmount: 'price',
-      createExchangeTxn: 'create', // Has a body
+      getMinimumAmount: 'price', // query params: from, to, amount
+      createExchangeTxn: 'create', // POST with body
     },
   },
   {
     name: 'swapuz',
-    baseUrl: '',
+    baseUrl: '', // e.g., 'https://api.swapuz.com/'
     apiKey: process.env.SWAPUZ_API_KEY || '',
     endpoints: {
       tokens: 'home/v1/coins',
-      chains: '',
-      getMiniumAmount: '',
-      getTransactions: 'home/v1/rate/', // has a coin query which is ?from, to , amount , fromNetwork toNetwork and mode
-      getTransactionById: 'order/uid/', // will have uuid has a param
-      createExchangeTxn: 'home/v1/order',
+      getTransactions: 'home/v1/rate/', // query: from, to, amount, fromNetwork, toNetwork, mode
+      getTransactionById: 'order/uid/', // expects UUID as param
+      createExchangeTxn: 'home/v1/order', // POST with body
     },
   },
   {
     name: 'simple_swap',
-    baseUrl: '',
+    baseUrl: '', // e.g., 'https://api.simpleswap.io/v1/'
     apiKey: process.env.SIMPLE_SWAP_API_KEY || '',
     endpoints: {
       tokens: 'pairs',
       chains: 'currencies/networks',
-      getTransactions: 'exchanges', // Hhas some queries like ?from, to, amount, fromNetwork, toNetwork
-      getTransactionById: 'exchanges/', // Expects a public Id
-      createExchangeTxn: 'exchanges', // Has a body
-      confirmTransaction: 'exchanges', // Has a body
-      getMiniumAmount: 'ranges',
+      getTransactions: 'exchanges', // query: from, to, amount, fromNetwork, toNetwork
+      getTransactionById: 'exchanges/', // expects public ID as param
+      createExchangeTxn: 'exchanges', // POST with body
+      confirmTransaction: 'exchanges', // POST with body
+      getMinimumAmount: 'ranges',
     },
   },
   {
     name: 'changenow',
-    baseUrl: '',
+    baseUrl: '', // e.g., 'https://api.changenow.io/v1/'
     apiKey: process.env.CHANGENOW_API_KEY || '',
     endpoints: {
-      tokens: 'pairs',
-      chains: 'currencies/networks',
-      getTransactions: 'exchanges', // Hhas some queries like ?from, to, amount, fromNetwork, toNetwork
-      getTransactionById: 'exchanges/', // Expects a public Id
-      confirmTransaction: 'exchanges', // Has a body
-      createExchangeTxn: '',
-      getMiniumAmount: 'ranges',
+      tokens: 'currencies?active=true&fixedRate=true',
+      getTransactions: 'exchanges', // query: from, to, amount, fromNetwork, toNetwork
+      getTransactionById: 'exchanges/', // expects public ID as param
+      confirmTransaction: 'exchanges', // POST with body
+      getMinimumAmount: 'ranges',
     },
   },
 ];
