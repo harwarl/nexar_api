e4f1d9e69a7dcfb590e9fefccf1ecc323768dbe9
harwarl <harwarl87@gmail.com>
2025-08-12 21:55:05 +0100

    chore: get and post provider

--- src/swapv2/affiliate.service.ts
+++ src/swapv2/affiliate.service.ts
@@ -1,8 +1,10 @@
 import { HttpService } from '@nestjs/axios';
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Model } from 'mongoose';
 import { Transaction } from './types/transaction.interface';
+import { firstValueFrom } from 'rxjs';
+import { AFFILIATE_PROVIDERS, AffiliateProviderConfig } from './affiliateData';
 
 @Injectable()
 export class AffiliateService {
@@ -19,5 +21,99 @@ export class AffiliateService {
     private transactionModel: Model<Transaction>,
   ) {}
 
-  // private functions
+  /**
+   * Get provider configuration by name.
+   * @param providerName - The name of the affiliate provider.
+   * @returns The configuration object for the specified provider.
+   * @throws HttpException if the provider is not found.
+   */
+  getProviderConfig(providerName: string): AffiliateProviderConfig {
+    const providers: AffiliateProviderConfig[] = AFFILIATE_PROVIDERS;
+    const provider = providers.find((p) => p.name === providerName);
+
+    if (!provider) {
+      throw new HttpException(`Provider ${providerName} not found`, 404);
+    }
+
+    return provider;
+  }
+
+  /**
+   * Make API Get request to the specified provider's endpoint.
+   */
+  private async getFromProvider(
+    providerName: string,
+    endpointKey: keyof (typeof AFFILIATE_PROVIDERS)[number]['endpoints'],
+    params?: Record<string, any>,
+  ): Promise<any> {
+    const provider = this.getProviderConfig(providerName);
+    const endpoint = provider.endpoints[endpointKey];
+
+    if (!endpoint) {
+      throw new HttpException(
+        `Endpoint ${endpointKey} not found for provider ${providerName}`,
+        404,
+      );
+    }
+
+    // Get the url
+    const url = `${provider.baseUrl}${endpoint}`;
+
+    // Set up headers
+    const headers: Record<string, string> = {};
+    if (provider.apiKey) {
+      headers['Authorization'] = `Bearer ${provider.apiKey}`;
+    }
+    if (provider.apiSecret) {
+      headers['x-api-key'] = provider.apiSecret;
+    }
+
+    // Make the HTTP Get request
+    const data = await firstValueFrom(
+      this.httpService.get(url, { params, headers }),
+    );
+
+    console.log({ data });
+    return data;
+  }
+
+  /**
+   * Make API Post request to the specified provider's endpoint.
+   */
+  private async postToProvider(
+    providerName: string,
+    endpointKey: keyof (typeof AFFILIATE_PROVIDERS)[number]['endpoints'],
+    body: any,
+  ) {
+    const provider = this.getProviderConfig(providerName);
+    const endpoint = provider.endpoints[endpointKey];
+
+    if (!endpoint) {
+      throw new HttpException(
+        `Endpoint ${endpointKey} not found for provider ${providerName}`,
+        404,
+      );
+    }
+
+    // Get the url
+    const url = `${provider.baseUrl}${endpoint}`;
+
+    // Set up headers
+    const headers: Record<string, string> = {};
+
+    if (provider.apiKey) {
+      body = { ...body, apiKey: provider.apiKey };
+    }
+
+    if (provider.apiSecret) {
+      headers['x-api-key'] = provider.apiSecret;
+    }
+
+    const { data } = await firstValueFrom(
+      this.httpService.post(url, body, { headers }),
+    );
+
+    console.log({ data });
+    return data;
+  }
 }
